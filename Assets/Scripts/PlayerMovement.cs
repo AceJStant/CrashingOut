314f53bf0ce31209bf8072da0796b34f13f763a7
AceJStant <acejstant@gmail.com>
2025-04-07 13:52:32 -0700

    PlayerMovement
    Changed slightly to playermovement cript

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/* Ace J. Stant
+ * 4/7/25
+ * Player Movement 
+ */
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Move as crash, move forward, backward, left, side
+    /// </summary>
+    private void CrashMovement()
+    {
+
+    }
+}
