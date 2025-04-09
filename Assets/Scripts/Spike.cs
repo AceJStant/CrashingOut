8a7871606cc0b88644c3b80467520dde9ceb07c8
nanonano909 <emilianobucio352@gmail.com>
2025-04-09 12:44:10 -0700

    spike created

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Check if colliding object was the player
+        if (collision.gameObject.GetComponent<PlayerMovement>())
+        {
+            //Have player lose a life 
+            collision.gameObject.GetComponent<PlayerMovement>().LoseLife();
+
+
+        }
+
+    }
+
+    //Cjecks for overlap with the player
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check if colliding object was the player
+        if (other.gameObject.GetComponent<PlayerMovement>())
+        {
+            //Have player lose a life 
+            other.gameObject.GetComponent<PlayerMovement>().LoseLife();
+
+
+        }
+
+
+
+
+    }
+}
