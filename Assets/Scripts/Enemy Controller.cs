228920e708b655a75f55660c87bae57e4930de9e
Cupcakefrosters <carterprestwich@gmail.com>
2025-04-12 12:26:17 -0700

    Different Enemy types
    adds enemy type script
    adds enemy type prefabs

--- Assets/Scripts/Enemy Controller.cs
+++ Assets/Scripts/Enemy Controller.cs
@@ -2,14 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Carter Prestwich
+//4/12/2025
+//Controls enemy variables like speed and types of enemies
+//You can change enemy pathing in the game scene
+
+//List the different enemy types, can be changed in the inspector 
+public enum EnemyType
+{
+    //Only dies from spin attack
+    SpikyTurtle,
+    //Only dies from jumping on top
+    Shielded,
+    // Dies from both jump and spin attack
+    RegularEnemy 
+}
+
 public class EnemyController : MonoBehaviour
 {
+    //Determines where the enemy will go to thier left
     public GameObject leftPoint;
+    //Determines where the enemy will go to thier right
     public GameObject rightPoint;
     private Vector3 leftPos;
     private Vector3 rightPos;
+    //Changes enemy speed in inspector
     public int speed;
+    //Tells if enemy moves to the left
     public bool goingLeft;
+    //Changes enemy type in inspector
+    public EnemyType enemyType;
 
     // Start is called before the first frame update
     void Start()
@@ -25,16 +47,20 @@ public class EnemyController : MonoBehaviour
     }
 
     /// <summary>
-    /// The Enemy movement code
+    /// The enemy movement code 
     /// </summary>
     private void EnemyMove()
     {
+        //Check the direction the enemy is currently moving
         if (goingLeft)
         {
+            //If the enmey has reached the left boundray
             if (transform.position.x <= leftPos.x)
             {
+                //Now go right
                 goingLeft = false;
             }
+            //If not keep going
             else
             {
                 transform.position += Vector3.left * Time.deltaTime * speed;
@@ -42,67 +68,137 @@ public class EnemyController : MonoBehaviour
         }
         else
         {
+            //If the enmey has reached the right boundray
             if (transform.position.x >= rightPos.x)
             {
+                //Now go left
                 goingLeft = true;
             }
             else
             {
+                //If not keep going
                 transform.position += Vector3.right * Time.deltaTime * speed;
             }
         }
     }
 
     /// <summary>
-    /// Enemy Damages player script
+    /// See if collision is with the player, then play out effects for different enemy types
     /// </summary>
-    /// <param name="collision"></param>
-    //Check for physical collisions with the player
     private void OnCollisionEnter(Collision collision)
     {
-        //Check if the colliding object has the player script
         if (collision.gameObject.GetComponent<PlayerMovement>())
         {
-            //Have the Player lose a Life
-            collision.gameObject.GetComponent<PlayerMovement>().LoseLife();
+            //Gets the playermovement that collided with the enemy to see if they are spinning or should loselife
+            PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
+            //This will see the first part of contact betwee the player and enemies collision
+            ContactPoint contact = collision.GetContact(0);
+            //Checks if the players is above the enemy, can use boxcollider or a rigidbody to determine
+            bool jumpedOn = contact.normal.y > 0.5f;
+
+            // Check collision if player is spin attacking
+            if (player.spinning)  
+            {
+                //SpikyTurtle dies from spin attack
+                if (enemyType == EnemyType.SpikyTurtle)
+                {
+                    Die(); 
+                }
+                // RegularEnemy dies from spin attack
+                else if (enemyType == EnemyType.RegularEnemy)
+                {
+                    Die(); 
+                }
+            }
+            //Checks collision when player is not spinning
+            else
+            {
+                // SpikyTurtle kills the player if not spinning
+                if (enemyType == EnemyType.SpikyTurtle)
+                {
+                    player.LoseLife(); 
+                }
+                // Shielded dies if jumped on
+                else if (enemyType == EnemyType.Shielded)
+                {
+                    if (jumpedOn)
+                    {
+                        Die();
+                    }
+                    // Shielded kills player if attacked
+                    else
+                    {
+                        player.LoseLife(); 
+                    }
+                        
+                }
+                //Checks if player is either jumping or spin attacking regualr enemy
+                else if (enemyType == EnemyType.RegularEnemy)
+                {
+                    //regular enemy dies
+                    if (jumpedOn || player.spinning)
+                    {
+                        Die();
+                    }
+                    else
+                    {
+                        player.LoseLife();
+                    }
+                }
+            }
         }
     }
 
-    //Check for overlap with the Player
+    /// <summary>
+    /// See if trigger is with the player, then play out effects for different enemy types
+    /// </summary>
     private void OnTriggerEnter(Collider other)
     {
-        //Check if the colliding object has the player script
         if (other.gameObject.GetComponent<PlayerMovement>())
         {
-            //Have the Player lose a Life
-            other.gameObject.GetComponent<PlayerMovement>().LoseLife();
+            PlayerMovement player = other.GetComponent<PlayerMovement>();
+
+            // Check if player is spin attacking
+            if (player.spinning)
+            {
+                // SpikyTurtle dies from spin attack
+                if (enemyType == EnemyType.SpikyTurtle)
+                {
+                    Die(); 
+                }
+                // RegularEnemy dies from attack
+                else if (enemyType == EnemyType.RegularEnemy)
+                {
+                    Die(); 
+                }
+            }
+            // Handle collision when player is not spinning
+            else
+            {
+                // Shielded kills player if not jumping
+                if (enemyType == EnemyType.Shielded)
+                {
+                    player.LoseLife(); 
+                }
+                // RegularEnemy dies from attack
+                else if (enemyType == EnemyType.RegularEnemy)
+                {
+                    if (player.spinning)
+                    {
+                        Die();
+                    }
+                    else
+                    {
+                        player.LoseLife();
+                    }
+                }
+            }
         }
     }
 
-    //Have tags for the three enemy types - regular, Spiked, and Sheild
-    //Have enemys get gameobject or tag of spin, to tell if the enemy dies to spin attack from player
-    //Have enemys get gameobject of player, to tell if the enemy dies to player being above it
-    /*
-    public void EnemyTypes()
+    //Enemy will despawn if correct attack is used
+    private void Die()
     {
-        //If the enemy Has the tag "Regular Enemy" then it can be interacted by spin and jump
-        if(GameObject.GetCommponent == "Regular Enemy")
-        {
-
-        }
-
-        //If the enemy has the tag "Sheilded Enemy" then it can be interacted by only jumps from the player
-        if (GameObject.GetCommponent == "Sheilded Enemy")
-        {
-
-        }
-
-        //If the enemy has the tag "Spiky Turtle Enemy" then it can be interacted by only spin from the player
-        if (GameObject.GetCommponent == "Spiky Turtle Enemy")
-        {
-
-        }
+        Destroy(gameObject);
     }
-    */
 }
-
